738d19142147cfc1c4589d8557c6824eb6867652
xXMaKuSXx <93930187+xXMaKuSXx@users.noreply.github.com>
2022-12-29 04:20:22 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,6 +3,25 @@ const { isEmail } = require('validator')
 const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Please enter an username'],
+        unique: true,
+        minLength: [3, 'Minimum username length is 3 characters']
+    },
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    phonenumber: {
+        type: String,
+        required: [true, 'Please enter a phone number'],
+        unique: true
+    },
     email: {
         type: String,
         required: [true, 'Please enter an email'],
@@ -15,7 +34,45 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Please enter an password'],
         minLength: [6, 'Minimum password length is 6 characters']
     },
+    phonenumber:{
+        type: String,
+        required: [true, 'Please enter a phone number'],
+        unique: true
+    }
 });
+
+userSchema.statics.isThisPhoneInUse = async function(phonenumber){
+    try {
+        const user = await this.findOne({phonenumber})
+        if(user) {
+            console.log('Phone number already used');
+            return true
+        }
+    }   catch(error) {
+        console.log('already used', error.message)
+        return false
+    }
+}
+
+userSchema.statics.isThisPhoneValid = async function(phonenumber){
+    validPhone = false
+    if((phonenumber < 999999999) && (phonenumber > 100000000) || (phonenumber < +48999999999) && (phonenumber > +48100000000))
+        validPhone = true
+    return validPhone
+}
+
+userSchema.statics.isThisUsernameInUse = async function(username){
+    try {
+        const user = await this.findOne({username})
+        if(user) {
+            console.log('Username used try another');
+            return true
+        }
+    }   catch(error) {
+        console.log('already used', error.message)
+        return false
+    }
+}
 
 userSchema.post('save', function (doc, next){
     console.log("new user was created & saved", doc);
@@ -29,9 +86,9 @@ userSchema.pre('save', async function (next) {
     next();
 })
 
-// static methon to login user
-userSchema.statics.login = async function(email, password) {
-    const user = await this.findOne( {email });
+// static method to login user
+userSchema.statics.login = async function(username, password) {
+    const user = await this.findOne( {username });
     if (user) {
         const auth = await bcrypt.compare(password, user.password);
         if (auth) {
@@ -39,7 +96,7 @@ userSchema.statics.login = async function(email, password) {
         }
         throw Error('incorrect password');
     }
-    throw Error('incorrect email');
+    throw Error('incorrect username');
 }
 
 const User = mongoose.model('user', userSchema);
